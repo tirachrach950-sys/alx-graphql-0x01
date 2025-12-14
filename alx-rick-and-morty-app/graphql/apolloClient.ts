46e440acf5cf7c08288b93b4de0944ee4ee40106
tirachrach950-sys <tirachrach950@gmail.com>
2025-12-14 17:41:32 +0100

    Update apolloClient.ts

--- alx-rick-and-morty-app/graphql/apolloClient.ts
+++ alx-rick-and-morty-app/graphql/apolloClient.ts
@@ -1,1 +1,11 @@
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://rickandmortyapi.com/graphql",
+  }),
+  cache: new InMemoryCache(),
+});
+
+export default client;
 
