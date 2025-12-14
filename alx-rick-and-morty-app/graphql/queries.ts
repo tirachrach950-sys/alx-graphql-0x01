b11bf795d7ee29d73a0fb8db501722ba8de529e4
tirachrach950-sys <tirachrach950@gmail.com>
2025-12-14 17:41:55 +0100

    Update queries.ts

--- alx-rick-and-morty-app/graphql/queries.ts
+++ alx-rick-and-morty-app/graphql/queries.ts
@@ -1,1 +1,21 @@
+import { gql } from "@apollo/client";
+
+export const GET_EPISODES = gql`
+  query getEpisodes($page: Int, $filter: FilterEpisode) {
+    episodes(page: $page, filter: $filter) {
+      info {
+        pages
+        next
+        prev
+        count
+      }
+      results {
+        id
+        name
+        air_date
+        episode
+      }
+    }
+  }
+`;
 
